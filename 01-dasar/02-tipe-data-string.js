6d1b9c831df02cdf9d6f446e5c589b83c1bb6e14
gsoul94 code <gsoul94.code@gmail.com>
2022-04-03 03:31:36 +0800

    update

---
+++ 01-dasar/02-tipe-data-string.js
@@ -0,0 +1,17 @@
+document.writeln("Hello!");
+document.writeln("</br>");
+document.writeln("Gsoul94");
+
+// penggabungan string
+document.writeln("</br>");
+document.writeln("Hello!, Gsoul94");
+
+// escape sequence
+document.writeln("</br>");
+document.writeln("</br>");
+document.writeln("<textarea cols='100' rows='10'>");
+document.write("Gsoul94\n- Belajar Javascript\n");
+document.write("\\Home\\User\\Gsoul94\n");
+document.write("\"Gsoul94 Code\"\n");
+document.write("\'Belajar Javascript\'");
+document.writeln("</textarea>");
---
+++ 01-dasar/03-tipe-data-number.js
@@ -0,0 +1,13 @@
+document.writeln("number ", 100);
+document.writeln("</br>");
+document.write("pecahan ", 1.2);
+
+// binary
+document.writeln("</br>");
+document.writeln("binary ", 0b101101);
+// hexadecimal
+document.writeln("</br>");
+document.writeln("hexadecimal ", 0xFF);
+// octal
+document.writeln("</br>");
+document.writeln("octal ", 0o10);
